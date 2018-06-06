488bb32e6a042bb33df97ea51b9412ce7198b1e4
NicholasHaven <nicholas.haven@trivantis.com>
2018-06-06 15:31:13 -0400

    Update UptimeServiceWithLocalComputerIPCheckSlackAlert.sh

--- UptimeServiceWithLocalComputerIPCheckSlackAlert.sh
+++ UptimeServiceWithLocalComputerIPCheckSlackAlert.sh
@@ -1,18 +1,23 @@
 #!/bin/sh
-server=( 127.0.0.1 ) ###put IP addresses in this field, space delimited
-ipaddy=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/    <.*$    //') ###This finds your public IP
-if [[ $ipaddy == "YourIPaddress"*  ||  $ipaddy == "YourOtherIPaddress"* ]] > /dev/null ; then ### Muliple IPs can be specified 
+###put IP addresses in this field, space delimited
+server=( 127.0.0.1 )
+###This finds your public IP
+ipaddy=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/    <.*$    //')
+### Muliple IPs can be specified
+if [[ $ipaddy == "YourIPaddress"*  ||  $ipaddy == "YourOtherIPaddress"* ]] > /dev/null ; then 
 #echo $ipaddy
 for i in "${server[@]}" ; do
-if ssh -qn root@$i  -o IdentityFile='/Path/To/Key' "service crond status | grep running" > /dev/null ; then ###This checks to see if cron is running
+###This checks to see if cron is running, Change Key
+if ssh -qn root@$i  -o IdentityFile='/Path/To/Key' "service crond status | grep running" > /dev/null ; then
 	#uncomment out the line below while testing
 	#echo "Turned on"
 	:
 else
 	#uncomment out the line below while testing
 	#echo "Not Turned on"
-	#comment out the below line while server is being maintenced##
+	###Change Key
 	ssh -qn root@$i  -o IdentityFile='/Path/To/Key' "service crond start"
+	### Slack Notification
 	curl -X POST --data-urlencode 'payload={"channel": "#ChannelName", "YourSlackUserName": "webhookbot", "text": "Cron was turned on for '$i'", "icon_emoji": ":tada:"}' https://hooks.slack.com/services/hookID/HookIDtag
 fi
 	wait
